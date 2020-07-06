bd028c1d825b8d292f2b2d1603274e909b98e590
mycaiye <1783777329@qq.com>
2020-07-06 19:05:45 +0800

    add

--- Notebooks-Python1.py
+++ Notebooks-Python1.py
@@ -590,9 +590,246 @@ print(main(data))
 
 
 
-7 asyncio 协程
-实例
-async def caiye():
-    # test
-    pass
+7 asyncio python的协程模块
+协程(coroutine) ---- 本质上就是一个函数， 怎么判断一个函数是不是协程？通过asyncio.iscoroutine（obj）和asyncio.iscoroutinefunction(func)加以判断，返回true，则是。
+
+
+一， 事件循环
+协程函数，不是像普通函数那样直接调用运行，必须要注册到事件循环中，然后由事件循环去运行，单独运行协程函数是不会有结果的
+看下面这个例子：
+import time
+import asyncio
+async def say_after_time(delay,what):
+        await asyncio.sleep(delay)
+        print(what)
+ 
+async def main():
+        print(f"开始时间为： {time.time()}")
+        await say_after_time(1,"hello")
+        await say_after_time(2,"world")
+        print(f"结束时间为： {time.time()}")
+ 
+loop=asyncio.get_event_loop()    #创建事件循环对象
+#loop=asyncio.new_event_loop()   #与上面等价，创建新的事件循环
+loop.run_until_complete(main())  #通过事件循环对象运行协程函数
+loop.close()
+
+（1）获取事件循环对象的几种方式：
+
+下面几种方式可以用来获取、设置、创建事件循环对象loop
+
+loop=asyncio.get_running_loop() 返回（获取）在当前线程中正在运行的事件循环，如果没有正在运行的事件循环，则会显示错误；它是python3.7中新添加的
+
+loop=asyncio.get_event_loop() 获得一个事件循环，如果当前线程还没有事件循环，则创建一个新的事件循环loop；
+
+loop=asyncio.set_event_loop(loop) 设置一个事件循环为当前线程的事件循环；
+
+loop=asyncio.new_event_loop() 创建一个新的事件循环
+
+（2）通过事件循环运行协程函数的两种方式：
+
+（1）方式一：创建事件循环对象loop，即asyncio.get_event_loop()，通过事件循环运行协程函数
+
+（2）方式二：直接通过asyncio.run(function_name)运行协程函数。但是需要注意的是，首先run函数是python3.7版本新添加的，前面的版本是没有的；
+其次，这个run函数总是会创建一个新的事件循环并在run结束之后关闭事件循环，
+所以，如果在同一个线程中已经有了一个事件循环，则不能再使用这个函数了，因为同一个线程不能有两个事件循环，
+而且这个run函数不能同时运行两次，因为他已经创建一个了。即同一个线程中是不允许有多个事件循环loop的。
+
+asyncio.run（）是python3.7 新添加的内容，也是后面推荐的运行任务的方式，因为它是高层API，后面会讲到它与asyncio.run_until_complete()的差异性，run_until_complete()是相对较低层的API。
+
+注意：到底什么是事件循环？如何理解？
+
+可以这样理解：线程一直在各个协程方法之间永不停歇的游走，遇到一个yield from 或者await就悬挂起来，然后又走到另外一个方法，依次进行下去，知道事件循环所有的方法执行完毕。
+实际上loop是BaseEventLoop的一个实例，我们可以查看定义，它到底有哪些方法可调用。
+
+
+二， 什么是awaitable对象——即可暂停等待的对象
+
+有三类对象是可等待的，即 coroutines, Tasks, and Futures.
+coroutine：本质上就是一个函数，一前面的生成器yield和yield from为基础，不再赘述；
+Tasks: 任务，顾名思义，就是要完成某件事情，其实就是对协程函数进一步的封装；
+Future：它是一个“更底层”的概念，他代表一个一步操作的最终结果，因为一步操作一般用于耗时操作，结果不会立即得到，会在“将来”得到异步运行的结果，故而命名为Future。
+三者的关系，coroutine可以自动封装成task，而Task是Future的子类。
+
+
+三，什么是task任务
+如前所述，Task用来 并发调度的协程，即对协程函数的进一步包装？那为什么还需要包装呢？
+因为单纯的协程函数仅仅是一个函数而已，将其包装成任务，任务是可以包含各种状态的，异步编程最重要的就是对异步操作状态的把控了。
+
+（1）创建任务（两种方法）：
+方法一：task = asyncio.create_task(coro())   # 这是3.7版本新添加的
+方法二：task = asyncio.ensure_future(coro())
+也可以使用
+loop.create_future()
+loop.create_task(coro)
+
+（2）获取某一个任务的方法：
+方法一：task=asyncio.current_task(loop=None)
+返回在某一个指定的loop中，当前正在运行的任务，如果没有任务正在运行，则返回None；
+如果loop为None，则默认为在当前的事件循环中获取，
+方法二：asyncio.all_tasks(loop=None)
+返回某一个loop中还没有结束的任务
+
+task 详解：
+（1）他是作为一个python协程对象，和Future对象很像的这么一个对象，但不是线程安全的；他继承了Future所有的API，，除了Future.set_result()和Future.set_Exception()；
+（2）使用高层API  asyncio.ccreate_task()创建任务，或者是使用低层API loop.create_task()或者是loop.ensure_future()创建任务对象；
+（3）相比于协程函数，任务时有状态的，可以使用Task.cancel()进行取消，这会触发CancelledError异常，使用cancelled()检查是否取消。
+
+
+四，什么是future？
+Future是一个较低层的可等待（awaitable）对象，他表示的是异步操作的最终结果，当一个Future对象被等待的时候，协程会一直等待，直到Future已经运算完毕。
+Future是Task的父类，一般情况下，已不用去管它们两者的详细区别，也没有必要去用Future，用Task就可以了，
+返回 future 对象的低级函数的一个很好的例子是 loop.run_in_executor().
+
+
+
+8， python asyncio wait和gather
+
+两者都是在协程需要并发的时候使用。
+
+wait接受一个协程列表，返回done, peding两个集合，done里面是完成任务的协程，pending表示仍在跑的协程，通过协程.result()的方法来获取完成的结果。<coroutine object wait at 0x1095a17c8>
+gather以gather(cro1, cro2, cro3, cro4…)的方式接受协程，返回的是一个结合了这么多个任务的协程<_GatheringFuture pending>，如果协程有返回值，则会返回协程充公运行之后的结果。
+
+async def func1(num):
+    print('--func1 start--')
+    await asyncio.sleep(num)
+    print('--func1 done--')
+    return 'func1 ok'
+
+async def func2(num):
+    print('--func2 start--')
+    await asyncio.sleep(num)
+    print('--func2 done--')
+    return 'func2 ok'
+
+async def main():
+    task1 = asyncio.ensure_future(func1(3))
+    task2 = asyncio.ensure_future(func2(5))
+    tasks = [task1, task2]
+    res = await asyncio.gather(*tasks)
+    return res
+    # done, pending = await asyncio.wait(tasks)
+    # for t in done:
+    #     print(t.result())
+    # print(done)
+    # print(pending)
+
+if __name__ == '__main__':
     
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(main())
+    print(result)
+
+
+9，获取协程的返回值
+
+（1）直接通过result() 获取
+async def caiye(sleep_time):
+    print('i will start!')
+    await asyncio.sleep(sleep_time)
+    print('ending...')
+    return 'caiye'
+
+loop = asyncio.get_event_loop()
+task = [asyncio.ensure_future(caiye(3))]
+loop.run_until_complete(asyncio.wait(task))
+for i in task:
+    print(i.result())
+# 输出
+i will start!
+ending...
+caiye  # return返回值
+
+（2）通过定义回调函数
+
+import asyncio
+import time
+ 
+ 
+async def hello1(a,b):
+    print("Hello world 01 begin")
+    await asyncio.sleep(3)  #模拟耗时任务3秒
+    print("Hello again 01 end")
+    return a+b
+ 
+def callback(future):   #定义的回调函数
+    print(future.result())
+ 
+loop = asyncio.get_event_loop()                #第一步：创建事件循环
+task=asyncio.ensure_future(hello1(10,5))       #第二步:将多个协程函数包装成任务
+task.add_done_callback(callback)                      #并被任务绑定一个回调函数
+ 
+loop.run_until_complete(task)                  #第三步：通过事件循环运行
+loop.close()                                   #第四步：关闭事件循环
+# '''运行结果为：
+# Hello world 01 begin
+# Hello again 01 end
+# 15
+
+
+10，协程标准模板
+（四步走）（针对python3.7之前的版本）
+第一步·：构造事件循环
+loop=asyncio.get_running_loop() #返回（获取）在当前线程中正在运行的事件循环，如果没有正在运行的事件循环，则会显示错误；它是python3.7中新添加的
+loop=asyncio.get_event_loop() #获得一个事件循环，如果当前线程还没有事件循环，则创建一个新的事件循环loop；
+loop=asyncio.set_event_loop(loop) #设置一个事件循环为当前线程的事件循环；
+loop=asyncio.new_event_loop()  #创建一个新的事件循环
+
+第二步：将一个或者是多个协程函数包装成任务Task
+#高层API
+task = asyncio.create_task(coro(参数列表))   # 这是3.7版本新添加的
+task = asyncio.ensure_future(coro(参数列表)) 
+ 
+#低层API
+loop.create_future(coro)
+loop.create_task(coro)
+
+
+第三步：通过事件循环运行
+loop.run_until_complete(asyncio.wait(tasks))  #通过asyncio.wait()整合多个task
+loop.run_until_complete(asyncio.gather(tasks))  #通过asyncio.gather()整合多个task
+loop.run_until_complete(task_1)  #单个任务则不需要整合
+loop.run_forever()  #但是这个方法在新版本已经取消，不再推荐使用，因为使用起来不简洁
+ 
+
+# 使用gather或者wait可以同时注册多个任务，实现并发,但他们的设计是完全不一样的，在前面的2.1.(4)中已经讨论过了，主要区别如下：
+# （1）参数形式不一样
+# gather的参数为 *coroutines_or_futures,即如这种形式
+#       tasks = asyncio.gather(*[task1,task2,task3])或者
+#       tasks = asyncio.gather(task1,task2,task3)
+#       loop.run_until_complete(tasks)
+# wait的参数为列表或者集合的形式，如下
+#       tasks = asyncio.wait([task1,task2,task3])
+#       loop.run_until_complete(tasks)
+# （2）返回的值不一样
+# gather的定义如下，gather返回的是每一个任务运行的结果，
+#       results = await asyncio.gather(*tasks) 
+# wait的定义如下,返回dones是已经完成的任务，pending是未完成的任务，都是集合类型
+#  done, pending = yield from asyncio.wait(fs)
+
+
+第四步：关闭事件循环
+
+loop.close()
+ 
+'''
+以上示例都没有调用 loop.close，好像也没有什么问题。所以到底要不要调 loop.close 呢？
+简单来说，loop 只要不关闭，就还可以再运行：
+loop.run_until_complete(do_some_work(loop, 1))
+loop.run_until_complete(do_some_work(loop, 3))
+loop.close()
+但是如果关闭了，就不能再运行了：
+loop.run_until_complete(do_some_work(loop, 1))
+loop.close()
+loop.run_until_complete(do_some_work(loop, 3))  # 此处异常
+建议调用 loop.close，以彻底清理 loop 对象防止误用
+'''
+
+两步走（针对python3.7）
+第一步：构建一个入口函数main
+他也是一个异步协程函数，即通过async定义，并且要在main函数里面await一个或者是多个协程，
+同前面一样，我可以通过gather或者是wait进行组合，对于有返回值的协程函数，一般就在main里面进行结果的获取。
+
+第二步：启动主函数main
+这是python3.7新添加的函数，就一句话，即
+asyncio.run(main())
